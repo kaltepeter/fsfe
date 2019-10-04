f15345e5f116bf8197e7a1ffe39a2c1c938cf92f
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-10-04 14:15:48 -0500

    update html

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = 3000;
 
@@ -9,7 +10,15 @@ app.get("/", (req, res) => {
 app.get("/teatime", (req, res) => {
   res.status(418);
   res.setHeader("X-message", "yo i'm a teapot");
-  res.end("I'm a \uf0f4");
+  let resHtml = "<!DOCTYPE html>";
+  resHtml += "<html><head>";
+  resHtml +=
+    '<script src="https://kit.fontawesome.com/40d41ae67d.js" crossorigin="anonymous"></script></head>';
+  resHtml +=
+    '<body style="font-size: 30px;margin: 5em;text-align: center;color:#E64A19;background-color:#FFE082;">';
+  resHtml += '<i class="fas fa-10x fa-mug-hot"></i>';
+  resHtml += "</body></html>";
+  res.end(resHtml);
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
