4f074c13431447f143d2f7d511761edfb29dd442
Kayla Altepeter <kayla.altepeter@merrillcorp.com>
2019-10-04 13:04:52 -0500

    I'm a teapot

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-	res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/teatime", (req, res) => {
+  res.status(418);
+  res.setHeader("X-message", "yo i'm a teapot");
+  res.end();
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
